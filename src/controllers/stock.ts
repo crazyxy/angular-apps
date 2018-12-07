731c4eccbe34176859984c696849f07607399edf
crazyxy <xuey@microsoft.com>
2018-12-07 15:11:24 -0800

    init stock

--- src/controllers/stock.ts
+++ src/controllers/stock.ts
@@ -5,5 +5,9 @@ import _fetch from 'isomorphic-fetch';
 const iex = new IEXClient(_fetch);
 
 export let stock = (req: Request, res: Response) => {
-
+    iex.stockCompany(req.params.company).then(
+        (data) => {
+            res.send(data);
+        }
+    );
 };
